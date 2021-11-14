12843bd38a076bfa49f5b4122f2887de28ab0aeb
nhanvu0901 <75715771+nhanvu0901@users.noreply.github.com>
2021-11-14 14:07:43 +0700

    1 version

---
+++ script.js
@@ -0,0 +1,191 @@
+const config = {
+  draggable: true,
+  position: '',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  moveSpeed: 'slow',
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  sparePieces: true,
+  onSnapEnd: onSnapEnd
+}
+
+const game = new Chess();
+const board = Chessboard('myBoard', config);
+$('#startBtn').on('click', board.start)
+$('#clearBtn').on('click', board.clear) // sua
+$('#flipOrientationBtn').on('click', board.flip)
+let undo = document.getElementById("undo");
+
+undo.addEventListener("click",()=>{
+     takeback();
+  });
+
+
+  function takeback() { 
+    game.undo();//undo chi doi nua nuoc chay 2x undo doi 1 nuoc
+    game.undo();
+    board.position(game.fen());
+    updateStatus(); }
+  
+  
+const pawnEvalWhite =
+  [
+    [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [ 5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+    [ 1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+    [ 0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+    [ 0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+    [ 0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+    [ 0.5,  1.0,  1.0, -2.0, -2.0,  1.0,  1.0,  0.5],
+    [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+  ];
+
+const pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+const knightEval =
+  [
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+    [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+    [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+    [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+    [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+    [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+    [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+    [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+  ];
+
+const bishopEvalWhite =
+  [
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [-1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [-1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [-1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [-1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [-1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [-1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+  ];
+
+const bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+const rookEvalWhite =
+  [
+    [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [ 0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [-0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ 0.0,  0.0,  0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+  ];
+
+const rookEvalBlack = reverseArray(rookEvalWhite);
+
+const queenEval =
+  [
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [-1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [-1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [-0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ 0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [-1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [-1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+  ];
+
+const kingEvalWhite =
+  [
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [ 2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0],
+    [ 2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0]
+  ];
+
+const kingEvalBlack = reverseArray(kingEvalWhite);
+
+function reverseArray(array) {
+  return array.slice().reverse();
+}
+
+function onDragStart(source, piece, position, orientation) {
+  if (game.game_over()) return false;
+
+  if ( 
+  (piece.search(/^b/) !== -1) ||
+  (game.turn() === 'b' && piece.search(/^w/) !== -1) ||
+  (game.turn() === 'w' && piece.search(/^b/) !== -1)
+    ) {
+    return false;
+  }
+}
+
+function onDrop(source, target) {
+  removeGreySquares();
+  const move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q'
+  });
+
+  if (move === null) return 'snapback';
+
+  setTimeout(makeBestMove, 250);
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+
+
+
+
+
+
+
+// show move
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+
+function removeGreySquares () {
+  $('#myBoard .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+  var $square = $('#myBoard .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
+function onMouseoverSquare (square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+function onMouseoutSquare (square, piece) {
+  removeGreySquares()
+}
---
+++ sideBar.js
@@ -0,0 +1,81 @@
+//  slide show function   
+var slideIndex =0;
+let dot = document.querySelectorAll(".dot");
+let slides = document.querySelectorAll(".mySlides");
+showSlides(slideIndex);
+
+function showSlides(n){
+    var i;
+    
+
+    if(n >=slides.length){
+        slideIndex = 0;
+    }
+    if(n < 0){
+        slideIndex = slides.length-1;
+    }
+    for( i=0 ; i<slides.length ; i++){
+        slides[i].style.display = "none";
+        dot[i].classList.remove("active");
+    }
+    slides[slideIndex].style.display = "block";
+    dot[slideIndex].classList.add("active");
+}
+document.querySelector(".arrow-prev").addEventListener("click",()=>{
+    showSlides(slideIndex -=1);
+});
+document.querySelector(".arrow-next").addEventListener("click",()=>{
+    showSlides(slideIndex+=1);
+});
+function currentSlide(n){
+    showSlides(slideIndex =n);
+}
+
+
+
+
+document.getElementById("button").addEventListener('click',changeBoard);
+
+        function changeBoard(e){
+        e.preventDefault();
+        let boardGame = document.querySelector('.board-b72b1');
+
+
+        // let num = document.getElementById("number");
+        // let boardColor = document.getElementById("board-color");
+         let border = document.getElementById("border-color");
+         let border_Color = border.options[border.selectedIndex].value;
+        // let dotColor = document.getElementById("dot-color");
+            
+       
+        let image = slides[slideIndex].children[1];
+        console.log(image.getAttribute("src"));
+        document.body.style.background = "url("+image.getAttribute("src")+")";
+        document.body.style.backgroundBlendMode ="multiply";
+        document.body.style.backgroundColor = "rgb(189, 179, 179)";
+        document.body.style.backgroundAttachment = "fixed"; 
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundSize = "cover";
+        boardGame.style.border = `4px solid ${border_Color}`;
+        }
+
+
+let setting = document.querySelector(".setting");
+let sideBar = document.getElementById("sidebar");
+
+document.getElementById("menu-btn").addEventListener("click", ()=>{
+  
+   sideBar.style.transform = "translateX(1000px)";
+   setting.style.display = "block";
+});
+
+document.querySelector(".setting").addEventListener("click",()=>{
+    
+    
+    setting.style.display = "none";
+    sideBar.style.transform = "translateX(0px)";
+})
+function newGame() {
+    window.location = "index.html";
+  }
